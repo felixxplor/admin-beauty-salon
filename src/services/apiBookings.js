e0d826106347b3b8f4c92e057c5f32352ca7b9b3
felixxplor <nhtu.sgs@gmail.com>
2025-10-04 11:38:10 +1000

    Update apiBookings.js

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -9,30 +9,26 @@ export async function getBookings({ filter, sortBy, page }) {
       'id, created_at, date, startTime, endTime, numClients, status, totalPrice, staffId, phone, name, serviceIds, services(name), client(fullName, email)',
       { count: 'exact' }
     )
-
-  //FILTER
+  
   if (filter) query = query[filter.method || 'eq'](filter.field, filter.value)
-
-  //SORT
+  
   if (sortBy)
     query = query.order(sortBy.field, {
       ascending: sortBy.direction === 'asc',
     })
-
+  
+  // Only apply pagination if page is provided
   if (page) {
     const from = (page - 1) * PAGE_SIZE
     const to = from + PAGE_SIZE - 1
-
     query = query.range(from, to)
   }
-
+  
   const { data, error, count } = await query
-
   if (error) {
     console.error(error)
     throw new Error('Bookings could not be loaded')
   }
-
   return { data, count }
 }
 
